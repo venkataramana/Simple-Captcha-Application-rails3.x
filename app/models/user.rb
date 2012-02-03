b9460a4504e0cb1447152934575380ad4e5a239a
kvr.sbm <kvramana.hb@gmail.com>
2012-02-03 13:56:57 +0530

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+	apply_simple_captcha
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaptchaDemo::Application.config.secret_token = 'c1821d011d68039e8c6ab90bdaf9469bc3d7f3765dc28efb3f4728d98331874595411b941a7932cca5220532b6e99380afc028e80ab19971915311f26523785f'
